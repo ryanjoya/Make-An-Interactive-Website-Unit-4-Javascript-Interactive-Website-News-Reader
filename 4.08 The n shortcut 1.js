c527a77e38382c4b6cc5a58abe7a2256c95783b7
Ryan Joya <ryanjoya@users.noreply.github.com>
2016-04-08 20:05:32 -0500

    Create 4.08 The n shortcut 1.js

---
+++ 4.08 The n shortcut 1.js
@@ -0,0 +1,19 @@
+var main = function() {
+    $('.article').click(function(){
+        $('.article').removeClass('current');
+        $('.description').hide();
+        
+        $(this).addClass('current');
+        $(this).children('.description').show();
+    });
+};
+$(document).keypress(function(event) {
+      if(event.which === 111) {
+    $('.current').children('.description').toggle();
+  }
+  {
+      else if(event.which === 110) {
+
+      }
+});
+$(document).ready(main);
